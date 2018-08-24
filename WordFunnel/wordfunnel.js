d314e9b6c7c01c41e43a08e4952f8efcfa8ccddb
Adnan Hafeez <adnanhr81@gmail.com>
2018-08-24 10:38:30 +1000

    Word Funnel Done

--- WordFunnel/wordfunnel.js
+++ WordFunnel/wordfunnel.js
@@ -1,7 +1,24 @@
+
 function funnel(w1,w2){
 
   if((w1.length - 1) != w2.length) return false;
-
-  for()
+  let j = 0;
+  let i = 0;
+  while (i < w1.length) {
+      if(w1.charAt(i) != w2.charAt(j)){
+        i++;
+        if(i > j+1) return false;
+        continue;
+    }
+    j++;
+  }
+  return true;
 
 }
+
+console.log(funnel("leave","eave"));
+console.log(funnel("reset","rest"));
+console.log(funnel("dragoon","dragon"));
+console.log(funnel("eave","leave"));
+console.log(funnel("sleet","lets"));
+console.log(funnel("skiff","ski"));
